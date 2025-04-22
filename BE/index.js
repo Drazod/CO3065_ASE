bbf2a532e2ad31f80cf242da533758c5794ccd81
Nguyễn Ngô Huy <huy.nguyenngo13@hcmut.edu.vn>
2025-04-22 17:37:55 +0700

    Implement user authentication APIs with JWT

--- BE/index.js
+++ BE/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const roomRoutes = require('./routes/roomRoute');
+const authRoutes = require('./routes/authRoute'); 
 
 dotenv.config();
 connectDB();
@@ -11,7 +12,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// HEALTH-CHECK ROUTE
+app.get('/', (req, res) => {
+  res.send('API is running!');
+});
+
 app.use('/api', roomRoutes);
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
